0f4be748248bdd085badc4d590fe2dc897e32e9b
encok <enckkipronoh@gmail.com>
2022-03-02 15:21:05 +0300

    adding brand logo to the navigation section

--- index.php
+++ index.php
@@ -13,9 +13,12 @@
 </head>
 
 <body>
+        <style>
+            nav img{height:30px;width:40px}
+        </style>
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="img/" alt="" /></a>
+            <a class="navbar-brand" href="#"><img src="img/ds.png" alt="" /></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
